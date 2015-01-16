f835ee3f8506d9242186d76bd149d3d10cb77df5
Michael Pinkert <mpinkert@wisc.edu>
2015-01-16 11:31:07 -0600

    I added tests to the median function.

--- test_stats.py
+++ test_stats.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance
 
-from stats import mean, mode, std, var#, median
+from stats import mean, mode, std, var, median
 
 def test_mean1():
     obs = mean([0, 0, 0, 0])
@@ -27,6 +27,31 @@ def test_floating_mean1():
 
 # FIXME Put Median tests here
 
+def test_median():
+	obs = median([0, 0, 0, 0])
+	exp = 0
+	assert_equal(obs, exp)
+	
+def test_median2():
+	obs = median([3,2,4]))
+	exp = 3
+	assert_equal(obs, exp)
+	
+def test_median3():
+	obs = median([4, 2, 1, 3])
+	exp = 2.5
+	assert_equal(obs, exp)
+
+def test_median4():
+	obs = median([1,3,5,7])
+	exp = 4
+	assert_equal(obs, exp)
+	
+def test_median5():
+	obs = median([3, 5, 17/3.0, 8])
+	exp = 16/3.0
+	assert_almost_equal(obs, exp)
+	
 # FIXME Put Mode tests here
 
 def test_std1():
